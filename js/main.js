2386648bf4178c0adaa4b42760376e511ed1596f
Kamron or Камрон <kamrush6429@gmail.com>
2023-12-01 18:00:40 +0500

    bugs login

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 const swiper = new Swiper(".swiper-container", {
   // Optional parameters
-  direction: "vertical",
-  effect: "coverflow",
+  effect: "cards",
+  grabCursor: true,
+  centerdSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    stopOnLastSlide: true,
+    disableOnInteraction: true,
+  },
   mousewheel: true,
   slidesPerView: 1,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
-  },
-  loop: true,
-  autoplay: {
-    delay: 3000,
-    stopOnLastSlide: true,
-    desableOnInteraction: false,
   },
   speed: 700,
 });
